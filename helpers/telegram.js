89631e091f48f0aba8d055c38df28d71892e2257
René van Mil <rvanmil@me.com>
2015-07-05 17:37:24 +0200

    log telegram api errors

--- helpers/telegram.js
+++ helpers/telegram.js
@@ -33,6 +33,10 @@ var telegram = {
             // Log the response code
             telegramResponse.on('end', function() {
                 console.log('Telegram API response code: ' + telegramResponse.statusCode);
+                // Log the repsonse message if the status code is not OK
+                if (telegramResponse.statusCode !== 200) {
+                    console.log(output);
+                }
             });
         });
         // Handle error
